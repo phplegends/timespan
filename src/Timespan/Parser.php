9dc00d5b78eab6c8911439fc2fbd019944ef5208
Wallace Maxters <wallacemaxters@gmail.com>
2021-11-15 12:31:25 -0300

    Parser and Timespan class

---
+++ src/Timespan/Parser.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PHPLegends\Timespan;
+
+/**
+ * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
+*/
+class Parser
+{
+    public const TIME_FORMAT_MEMBERS = [
+        'HOURS'         => '%h',
+        'MINUTES'       => '%i',
+        'SECONDS'       => '%s',
+        'TOTAL_MINUTES' => '%I',
+        'SIGN'          => '%R',
+        'NEGATIVE_SIGN' => '%r',
+    ];
+
+    public const TIME_REPLACEMENTS = [
+        '%h' => '(?<h>\d+)',
+        '%i' => '(?<i>[0-5][0-9])',
+        '%s' => '(?<s>[0-5][0-9])',
+        '%R' => '(?<r>[\+\-]{1})',
+        '%r' => '(?<r>\-{1})?',
+    ];
+
+    public static function createTimespanFromFormat(string $format, string $value): Timespan
+    {
+        $units = static::createUnitsFromFormat($format, $value);
+
+        $time = new Timespan($units['h'], $units['i'], $units['s']);
+
+        if (static::isNegativeSign($units['r'])) {
+            $time->setSeconds(-$time->getSeconds());
+        }
+
+        return $time;
+    }
+
+    public static function replacementsFromTimestamp(Timespan $time): array
+    {
+        $units      = $time->getUnits();
+        $isNegative = $time->isNegative();
+
+        $formats = static::TIME_FORMAT_MEMBERS;
+
+        return [
+            $formats['HOURS']         => sprintf('%02d', $units['hours']),
+            $formats['MINUTES']       => sprintf('%02d', $units['minutes']),
+            $formats['SECONDS']       => sprintf('%02d', $units['seconds']),
+            $formats['TOTAL_MINUTES'] => sprintf('%02d', $units['total_minutes']),
+            $formats['SIGN']          => $isNegative ? '-' : '+',
+            $formats['NEGATIVE_SIGN'] => $isNegative ? '-' : null,
+        ];
+    }
+
+    public static function createUnitsFromFormat(string $format, string $value): array
+    {
+        if (! preg_match(static::createRegexFromFormat($format), $value, $units)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Invalid string format for "%s"',
+                $value
+            ));
+        }
+
+        return $units + [
+            'h' => 0,
+            'i' => 0,
+            's' => 0,
+            'r' => '+'
+        ];
+    }
+
+    public static function isValidFormat(string $format, string $value): bool
+    {
+        return preg_match(static::createRegexFromFormat($format), $value) > 0;
+    }
+
+    protected static function isNegativeSign($sign): bool
+    {
+        return $sign === '-';
+    }
+
+    public static function createRegexFromFormat(string $format): string
+    {
+        $regex = strtr($format, static::TIME_REPLACEMENTS);
+
+        return "/^$regex$/";
+    }
+}
---
+++ src/Timespan/Timespan.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace PHPLegends\Timespan;
+
+/**
+ * The timestamp class
+ *
+ * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
+ * */
+class Timespan
+{
+    public const DEFAULT_FORMAT        = '%r%h:%i:%s';
+    public const TIME_WITH_SIGN_FORMAT = '%R%h:%i:%s';
+
+    protected $seconds = 0;
+
+    public function __construct(int $hours = 0, int $minutes = 0, int $seconds = 0)
+    {
+        $this->setTime($hours, $minutes, $seconds);
+    }
+
+    public function setTime(int $hours = 0, int $minutes = 0, int $seconds = 0): self
+    {
+        return $this->setSeconds(
+            ($hours * 3600) + (60 * $minutes) + $seconds
+        );
+    }
+
+    public function setSeconds(int $seconds): self
+    {
+        $this->seconds = $seconds;
+
+        return $this;
+    }
+
+    public function setMinutes(int $minutes): self
+    {
+        return $this->setTime(0, $minutes, 0);
+    }
+
+    public function setHours(int $hours): self
+    {
+        return $this->setTime($hours, 0, 0);
+    }
+
+    public function addSeconds(int $seconds): self
+    {
+        return $this->setTime(0, 0, $this->seconds + $seconds);
+    }
+
+    public function addMinutes(int $minutes): self
+    {
+        return $this->setTime(0, $minutes, $this->seconds);
+    }
+
+    public function addHours(int $hours): self
+    {
+        return $this->setTime($hours, 0, $this->seconds);
+    }
+
+    public function getSeconds(): int
+    {
+        return $this->seconds;
+    }
+
+    public function asMinutes(): float
+    {
+        return $this->getSeconds() / 60;
+    }
+
+    public function asHours(): float
+    {
+        return $this->getSeconds() / 3600;
+    }
+
+    public function format(string $format = self::DEFAULT_FORMAT): string
+    {
+        return strtr($format, Parser::replacementsFromTimestamp($this));
+    }
+
+    public function __toString()
+    {
+        return $this->format();
+    }
+
+    public function diff(self $time, bool $absolute = true): self
+    {
+        $diff = $this->getSeconds() - $time->getSeconds();
+
+        return new static(0, 0, $absolute ? abs($diff) : $diff);
+    }
+
+    public function isNegative(): bool
+    {
+        return $this->seconds < 0;
+    }
+
+    public function add(int $hours = 0, int $minutes = 0, int $seconds = 0): self
+    {
+        return $this->addHours($hours)->addMinutes($minutes)->addSeconds($seconds);
+    }
+
+    public function getUnits(): array
+    {
+        $seconds = abs($this->getSeconds());
+
+        $time['hours'] = floor($seconds / 3600);
+
+        $time['minutes'] = floor(($seconds - ($time['hours'] * 3600)) / 60);
+
+        $time['seconds'] = floor($seconds % 60);
+
+        $time['total_minutes'] = ($time['hours'] * 60) + $time['minutes'];
+
+        return $time;
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->getSeconds() == 0;
+    }
+
+    public function addFromString(string $strtime)
+    {
+        return $this->addSeconds(strtotime($strtime, 0));
+    }
+
+    public static function createFromString(string $strtime): self
+    {
+        return (new static())->addFromString($strtime);
+    }
+
+    public static function createFormatFormat(string $format, string $value): self
+    {
+        return Parser::createTimespanFromFormat($format, $value);
+    }
+}
